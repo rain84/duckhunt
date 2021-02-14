63e39d41c31e6d6a76ba77fd8e6b809fbd6bbf41
rain84 <rainy_sky@mail.ru>
2021-02-14 17:57:26 +0300

    Added Background object

---
+++ src/game/static/background.ts
@@ -0,0 +1,16 @@
+import Phaser from 'phaser'
+import { SceneObject } from 'game/interfaces'
+
+export class Background extends SceneObject {
+	constructor(private src: string, private scene: Phaser.Scene) {
+		super()
+	}
+
+	preload() {
+		this.scene.load.image('bg', this.src)
+	}
+
+	create() {
+		this.scene.add.image(SceneObject.center.x, SceneObject.center.y, 'bg').setDepth(100)
+	}
+}
