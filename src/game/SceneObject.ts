13740c7cf476aca491f08079316eda025d5e8a14
rain84 <rainy_sky@mail.ru>
2021-02-14 19:36:27 +0300

    💫 Duck flight implemented SceneObject updated

--- src/game/SceneObject.ts
+++ src/game/SceneObject.ts
@@ -1,6 +1,8 @@
 import config from '../config.json'
 
 export abstract class SceneObject {
+	static width = config.width
+	static height = config.height
 	static center = {
 		x: config.width / 2,
 		y: config.height / 2,
--- src/game/actors/duck.ts
+++ src/game/actors/duck.ts
@@ -2,7 +2,7 @@ import Phaser from 'phaser'
 import { SceneObject } from 'game/SceneObject'
 
 type SpriteWithDynamicBody = Maybe<Phaser.Types.Physics.Arcade.SpriteWithDynamicBody>
-type Sprite = Maybe<Phaser.GameObjects.Sprite>
+// type Sprite = Maybe<Phaser.GameObjects.Sprite>
 // type StaticGroup = Maybe<Phaser.Physics.Arcade.StaticGroup>
 // type Group = Maybe<Phaser.Physics.Arcade.Group>
 // type CursorKeys = Maybe<Phaser.Types.Input.Keyboard.CursorKeys>
@@ -10,8 +10,9 @@ type Sprite = Maybe<Phaser.GameObjects.Sprite>
 enum Direction {
 	FORWARD,
 	BACKWARD,
-	UP_RIGHT,
-	UP_LEFT,
+	TOP,
+	TOP_RIGHT,
+	TOP_LEFT,
 	DOWN,
 }
 
@@ -23,25 +24,33 @@ enum Animation {
 }
 
 export class Duck extends SceneObject {
+	static width = 40
+	static height = 37
+
+	#duck: SpriteWithDynamicBody
+	#flight = {
+		speed: 100,
+		velocity: { x: 0, y: 0 },
+		direction: Direction.FORWARD,
+		animation: Animation.FORWARD,
+	}
+
 	constructor(private scene: Phaser.Scene) {
 		super()
 	}
 
-	#duck: SpriteWithDynamicBody
-	#flight = {
-		isFlippedX: false,
-		velocity: { x: 160, y: 0 },
-		animation: Animation.FORWARD,
-	}
-
 	preload() {
-		this.scene.load.spritesheet('duck', 'game-assets/duck.sprite120x148.png', { frameWidth: 40, frameHeight: 37 })
+		this.scene.load.spritesheet('duck', 'game-assets/duck.sprite120x148.png', {
+			frameWidth: Duck.width,
+			frameHeight: Duck.height,
+		})
 	}
 
 	create() {
 		const { scene } = this
-		this.#duck = scene.physics.add.sprite(0, SceneObject.center.y, '')
+		this.#duck = scene.physics.add.sprite(SceneObject.width, SceneObject.center.y, '')
 		this.#duck.setScale(1.5).setBounce(0.4).setCollideWorldBounds(true)
+		this.setDirection(Direction.BACKWARD)
 
 		const animations = [
 			{
@@ -65,25 +74,85 @@ export class Duck extends SceneObject {
 			{
 				key: Animation.DOWN,
 				frames: scene.anims.generateFrameNumbers('duck', { frames: [9, 10] }),
-				frameRate: 8,
+				frameRate: 4,
 				repeat: -1,
 			},
 		]
 		animations.forEach((config) => scene.anims.create(config))
 	}
 
-	// onChange(direction: Direction) {
-	// 	switch (direction) {
-	// 		case Direction.FORWARD:
-	// 			break
+	setDirection(direction: Direction) {
+		if (!this.#duck) return
 
-	// 		default:
-	// 			break
-	// 	}
-	// }
+		switch (direction) {
+			case Direction.FORWARD:
+				this.#flight.velocity.x = this.#flight.speed
+				this.#flight.velocity.y = 0
+				this.#flight.animation = Animation.FORWARD
+				this.#duck.setFlip(false, false)
+				break
+
+			case Direction.BACKWARD:
+				this.#flight.velocity.x = -this.#flight.speed
+				this.#flight.velocity.y = 0
+				this.#flight.animation = Animation.FORWARD
+				this.#duck.setFlip(true, false)
+				break
+
+			case Direction.TOP:
+				this.#flight.velocity.x = 0
+				this.#flight.velocity.y = -this.#flight.speed
+				this.#flight.animation = Animation.TOP
+				this.#duck.setFlip(false, false)
+				break
+
+			case Direction.TOP_LEFT:
+				this.#flight.velocity.x = -this.#flight.speed
+				this.#flight.velocity.y = -this.#flight.speed
+				this.#flight.animation = Animation.DIAGONAL
+				this.#duck.setFlip(true, false)
+				break
+
+			case Direction.TOP_RIGHT:
+				this.#flight.velocity.x = this.#flight.speed
+				this.#flight.velocity.y = -this.#flight.speed
+				this.#flight.animation = Animation.DIAGONAL
+				this.#duck.setFlip(false, false)
+				break
+
+			case Direction.DOWN:
+				this.#flight.velocity.x = 0
+				this.#flight.velocity.y = this.#flight.speed
+				this.#flight.animation = Animation.DOWN
+				this.#duck.setFlip(false, false)
+				break
+
+			default:
+				throw new Error('Wrong direction')
+		}
+		this.#flight.direction = direction
+	}
+
+	checkOnBounds() {
+		if (!this.#duck) return
+
+		const isOutOfBorder = {
+			right: SceneObject.width - this.#duck.x - Duck.width < 0,
+			left: this.#duck.x - Duck.width < 0,
+			top: this.#duck.y - Duck.height < 0,
+			bottom: SceneObject.height - this.#duck.y - Duck.height < 0,
+		}
+
+		if (isOutOfBorder.right) this.setDirection(Direction.TOP_LEFT)
+		if (isOutOfBorder.top) this.setDirection(Direction.DOWN)
+		if (isOutOfBorder.left) this.setDirection(Direction.FORWARD)
+		if (isOutOfBorder.bottom) this.setDirection(Direction.TOP_LEFT)
+	}
 
 	update() {
 		if (!this.#duck) return
+
+		this.checkOnBounds()
 
 		const flight = this.#flight
 		this.#duck.setVelocity(flight.velocity.x, flight.velocity.y).play(flight.animation, true)
